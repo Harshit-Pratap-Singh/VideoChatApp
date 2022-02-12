42417fef0ea23d1797d7cc0cdc5d18f39bd090af
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2022-02-12 13:22:23 +0530

    turn

--- Public/chat.js
+++ Public/chat.js
@@ -17,6 +17,7 @@ let iceServers={
         {urls:'stun:stun.l.google.com:19302'},
         {urls:'stun:stun2.l.google.com:19302'},
         {urls:'stun:stun3.l.google.com:19302'},
+        {url:'turn:turn01.hubl.in?transport=udp'},
         {
             url: 'turn:numb.viagenie.ca',
             credential: 'muazkh',
