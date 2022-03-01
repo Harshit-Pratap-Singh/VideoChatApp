6024b0837b378d97a3936bd914123e61556a7dcf
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2022-03-01 16:30:25 +0530

    testin diff port 1

--- Public/chat.js
+++ Public/chat.js
@@ -1,266 +1,265 @@
-let socket = io();
+// let socket = io("https://localhost:4000/");
+
+const socket = io("https://localhost:4000/", {
+  withCredentials: true,
+});
 
 let $ = (e) => document.querySelector(e);
 
-let videoLoby = $('#video-chat-lobby');
-let videoChat = $('.video-chat');
-let joinButton = $('#join');
-let peerVideo = $('#peer-video');
-let userVideo = $('#user-video');
-let roomName = $('#roomName');
-let creater=false;
+let videoLoby = $("#video-chat-lobby");
+let videoChat = $(".video-chat");
+let joinButton = $("#join");
+let peerVideo = $("#peer-video");
+let userVideo = $("#user-video");
+let roomName = $("#roomName");
+let creater = false;
 let rtcPeerConnection;
 let userStream;
-let iceServers={
-    iceServers:[
-        {urls:'stun:stun.services.mozilla.com'},
-        {urls:'stun:stun3.l.google.com:19302'},
-        {
-            urls: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        
-    ]
-}
+let iceServers = {
+  iceServers: [
+    { urls: "stun:stun.services.mozilla.com" },
+    { urls: "stun:stun3.l.google.com:19302" },
+    {
+      urls: "turn:numb.viagenie.ca",
+      credential: "muazkh",
+      username: "webrtc@live.com",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+  ],
+};
 
 // let endCall_user;
 
+joinButton.addEventListener("click", () => {
+  if (roomName.value === "") alert("Please Enter The Room Name");
+  else {
+    socket.emit("join", roomName.value);
+  }
+});
 
-joinButton.addEventListener('click', () => {
-    if (roomName.value === '') alert("Please Enter The Room Name");
-    else {
-        socket.emit('join', roomName.value);
-    }
-})
-
-socket.on('created', () => {
-    creater=true;
-    navigator.mediaDevices.getUserMedia({
-        audio: {'echoCancellation': true}, video: { facingMode: "user", width: 1280, height: 720}
+socket.on("created", () => {
+  creater = true;
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: { echoCancellation: true },
+      video: { facingMode: "user", width: 1280, height: 720 },
     })
-        .then((stream) => {
-            userStream=stream;
-            userVideo.srcObject = stream;
-            videoLoby.style = 'display: none;'
-            videoChat.style='display:flex'
-            userVideo.style='display:block'
-            $('.waiting').style='display:block'
-            userVideo.onloadedmetadata = (e) => {
-                userVideo.play();
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-socket.on('joined', () => {
-    navigator.mediaDevices.getUserMedia({
-        audio: {'echoCancellation': true}, video: { facingMode: "user" ,width: 1280, height: 720  }
+    .then((stream) => {
+      userStream = stream;
+      userVideo.srcObject = stream;
+      videoLoby.style = "display: none;";
+      videoChat.style = "display:flex";
+      userVideo.style = "display:block";
+      $(".waiting").style = "display:block";
+      userVideo.onloadedmetadata = (e) => {
+        userVideo.play();
+      };
     })
-        .then((stream) => {
-            userStream=stream;
-            userVideo.srcObject = stream;
-            videoLoby.style = 'display: none;'
-            videoChat.style='display:flex'
-            userVideo.style='display:block'
-            userVideo.onloadedmetadata = (e) => {
-                userVideo.play();
-            }
-            
-            socket.emit('ready',roomName.value);
-        }).catch((err) => {
-            console.log(err);
-        })
-})
-socket.on('full', () => {
-    alert('Room is full cannot join');
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
+socket.on("joined", () => {
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: { echoCancellation: true },
+      video: { facingMode: "user", width: 1280, height: 720 },
+    })
+    .then((stream) => {
+      userStream = stream;
+      userVideo.srcObject = stream;
+      videoLoby.style = "display: none;";
+      videoChat.style = "display:flex";
+      userVideo.style = "display:block";
+      userVideo.onloadedmetadata = (e) => {
+        userVideo.play();
+      };
 
-socket.on('ready', () => {
-    console.log('ready client');
-    if(creater){
-    console.log('ready creater');
+      socket.emit("ready", roomName.value);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+socket.on("full", () => {
+  alert("Room is full cannot join");
+});
 
-        rtcPeerConnection=new RTCPeerConnection(iceServers);
-        rtcPeerConnection.onicecandidate=onIceCandidateFunction;
-        rtcPeerConnection.ontrack=onTrackFunction;
-        $('.endCall').classList.remove('disable');
-        console.log((userStream.getTracks()[0]));
-        console.log((userStream.getTracks()[1]));
-        rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream);
-        rtcPeerConnection.addTrack(userStream.getTracks()[1],userStream);
-        rtcPeerConnection.createOffer().then((offer)=>{
-         rtcPeerConnection.setLocalDescription(offer)
-            socket.emit('offer',offer,roomName.value);
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-})
+socket.on("ready", () => {
+  console.log("ready client");
+  if (creater) {
+    console.log("ready creater");
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = onIceCandidateFunction;
+    rtcPeerConnection.ontrack = onTrackFunction;
+    $(".endCall").classList.remove("disable");
+    console.log(userStream.getTracks()[0]);
+    console.log(userStream.getTracks()[1]);
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcPeerConnection
+      .createOffer()
+      .then((offer) => {
+        rtcPeerConnection.setLocalDescription(offer);
+        socket.emit("offer", offer, roomName.value);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
 
-socket.on('candidate', (candidate) => {
-    let  iceCandidate=new RTCIceCandidate(candidate);
-    rtcPeerConnection.addIceCandidate(iceCandidate);
-})
+socket.on("candidate", (candidate) => {
+  let iceCandidate = new RTCIceCandidate(candidate);
+  rtcPeerConnection.addIceCandidate(iceCandidate);
+});
 
-socket.on('offer', (offer) => {
-    if(!creater){
-        rtcPeerConnection=new RTCPeerConnection(iceServers);
-        rtcPeerConnection.onicecandidate=onIceCandidateFunction;
-        rtcPeerConnection.ontrack=onTrackFunction;
-        rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream);
-        rtcPeerConnection.addTrack(userStream.getTracks()[1],userStream);
-        rtcPeerConnection.setRemoteDescription(offer);
-        rtcPeerConnection.createAnswer().then((answer)=>{
-        rtcPeerConnection.setLocalDescription(answer)
-            socket.emit('answer',answer,roomName.value);
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-})
+socket.on("offer", (offer) => {
+  if (!creater) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = onIceCandidateFunction;
+    rtcPeerConnection.ontrack = onTrackFunction;
+    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcPeerConnection.setRemoteDescription(offer);
+    rtcPeerConnection
+      .createAnswer()
+      .then((answer) => {
+        rtcPeerConnection.setLocalDescription(answer);
+        socket.emit("answer", answer, roomName.value);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
 
-socket.on('answer', (answer) => {
-    rtcPeerConnection.setRemoteDescription(answer);
+socket.on("answer", (answer) => {
+  rtcPeerConnection.setRemoteDescription(answer);
 
-    rtcPeerConnection.onconnectionstatechange =e=>{
-        console.log(rtcPeerConnection.connectionState);
-        switch(rtcPeerConnection.connectionState){
-        case "closed":
-        case "failed":
-        case 'disconnected':
-        console.log('Failed--->',e);
+  rtcPeerConnection.onconnectionstatechange = (e) => {
+    console.log(rtcPeerConnection.connectionState);
+    switch (rtcPeerConnection.connectionState) {
+      case "closed":
+      case "failed":
+      case "disconnected":
+        console.log("Failed--->", e);
         endCall_user();
         break;
-        default:
-            console.log("Listening Connection state");
+      default:
+        console.log("Listening Connection state");
     }
-    }
-    
-})
+  };
+});
 
-let onIceCandidateFunction=(event)=>{
-    if(event.candidate){
-        socket.emit('candidate', event.candidate,roomName.value);
-    }
+let onIceCandidateFunction = (event) => {
+  if (event.candidate) {
+    socket.emit("candidate", event.candidate, roomName.value);
+  }
+};
+
+let onTrackFunction = (event) => {
+  console.log("peervideo");
+  peerVideo.style = "display:block";
+  $(".waiting").style = "display:none";
+  peerVideo.srcObject = event.streams[0];
+  console.log("event-->", event);
+  console.log("streams-->", event.streams[0].getTracks());
+  console.log(peerVideo.srcObject.getTracks()[1].enabled);
+  peerVideo.onloadedmetadata = (e) => {
+    peerVideo.play();
+  };
+  console.log("min--> p", peerVideo.srcObject.getTracks()[0].enabled);
+  userVideo.classList.add("user-animate");
+};
+
+$(".muteVideo").addEventListener("click", () => {
+  userStream.getTracks()[1].enabled = false;
+  $(".muteVideo").classList.add("disable");
+  $(".unMuteVideo").classList.remove("disable");
+  $(".video-btn").classList.add("red");
+  $(".video-btn").classList.remove("white");
+});
+
+$(".unMuteVideo").addEventListener("click", () => {
+  userStream.getTracks()[1].enabled = true;
+  $(".unMuteVideo").classList.add("disable");
+  $(".muteVideo").classList.remove("disable");
+  $(".video-btn").classList.remove("red");
+  $(".video-btn").classList.add("white");
+});
+
+$(".muteMic").addEventListener("click", () => {
+  userStream.getTracks()[0].enabled = false;
+  $(".muteMic").classList.add("disable");
+  $(".unMuteMic").classList.remove("disable");
+  $(".mic-btn").classList.add("red");
+  $(".mic-btn").classList.remove("white");
+});
+
+$(".unMuteMic").addEventListener("click", () => {
+  userStream.getTracks()[0].enabled = true;
+  $(".unMuteMic").classList.add("disable");
+  $(".muteMic").classList.remove("disable");
+  $(".mic-btn").classList.remove("red");
+  $(".mic-btn").classList.add("white");
+});
+
+$(".endCall").addEventListener("click", () => {
+  socket.emit("endCall", roomName.value);
+
+  videoLoby.style = "display: flex;";
+  videoChat.style = "display:none";
+  userVideo.style = "display:none";
+  peerVideo.style = "display:none";
+
+  creater = false;
+
+  if (userVideo.srcObject) {
+    userVideo.srcObject.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+  if (peerVideo.srcObject) {
+    peerVideo.srcObject.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+
+  if (rtcPeerConnection) {
+    rtcPeerConnection.ontrack = null;
+    rtcPeerConnection.onicecandidate = null;
+    rtcPeerConnection.close();
+    rtcPeerConnection = null;
+  }
+});
+
+socket.on("endCall", endCall_user);
+
+function endCall_user() {
+  peerVideo.style = "display:none";
+  $(".waiting").style = "display:block";
+  userVideo.classList.remove("user-animate");
+
+  creater = true;
+
+  if (peerVideo.srcObject) {
+    peerVideo.srcObject.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+
+  if (rtcPeerConnection) {
+    rtcPeerConnection.ontrack = null;
+    rtcPeerConnection.onicecandidate = null;
+    rtcPeerConnection.close();
+    rtcPeerConnection = null;
+  }
 }
-
-let onTrackFunction=(event)=>{
-    console.log('peervideo');
-    peerVideo.style="display:block"
-    $('.waiting').style='display:none'
-    peerVideo.srcObject=event.streams[0];
-    console.log('event-->',event);
-    console.log('streams-->',event.streams[0].getTracks());
-    console.log(peerVideo.srcObject.getTracks()[1].enabled);
-    peerVideo.onloadedmetadata=(e)=>{
-        peerVideo.play();
-    }
-    console.log("min--> p",peerVideo.srcObject.getTracks()[0].enabled);
-    userVideo.classList.add('user-animate');
-}
-
-$('.muteVideo').addEventListener('click',()=>{
-    userStream.getTracks()[1].enabled=false;
-    $('.muteVideo').classList.add('disable');
-    $('.unMuteVideo').classList.remove('disable');
-    $(".video-btn").classList.add('red');
-    $(".video-btn").classList.remove('white');
-
-})
-
-$('.unMuteVideo').addEventListener('click',()=>{
-    userStream.getTracks()[1].enabled=true;
-    $('.unMuteVideo').classList.add('disable');
-    $('.muteVideo').classList.remove('disable');
-    $(".video-btn").classList.remove('red');
-    $(".video-btn").classList.add('white');
-
-})
-
-$('.muteMic').addEventListener('click',()=>{
-    userStream.getTracks()[0].enabled=false;
-    $('.muteMic').classList.add('disable');
-    $('.unMuteMic').classList.remove('disable');
-    $(".mic-btn").classList.add('red');
-    $(".mic-btn").classList.remove('white');
-})
-
-$('.unMuteMic').addEventListener('click',()=>{
-    userStream.getTracks()[0].enabled=true;
-    $('.unMuteMic').classList.add('disable');
-    $('.muteMic').classList.remove('disable');
-    $(".mic-btn").classList.remove('red');
-    $(".mic-btn").classList.add('white');
-
-})
-
-$(".endCall").addEventListener('click',()=>{
-
-    socket.emit('endCall',roomName.value);
-
-    videoLoby.style = 'display: flex;'
-    videoChat.style='display:none'
-    userVideo.style='display:none'
-    peerVideo.style="display:none"
-
-    creater=false
-
-   if(userVideo.srcObject){
-       userVideo.srcObject.getTracks().forEach(track => {
-           track.stop();
-       });
-   }
-   if(peerVideo.srcObject){
-    peerVideo.srcObject.getTracks().forEach(track => {
-        track.stop();
-        });
-    }
-
-    if(rtcPeerConnection){
-        rtcPeerConnection.ontrack=null;
-        rtcPeerConnection.onicecandidate=null;
-        rtcPeerConnection.close();
-        rtcPeerConnection=null;
-    }
-
-})
-
-socket.on('endCall', endCall_user)
-
- function endCall_user(){
-    peerVideo.style="display:none"
-    $('.waiting').style='display:block'
-    userVideo.classList.remove('user-animate');
-
-    creater=true;
-
-    if(peerVideo.srcObject){
-        peerVideo.srcObject.getTracks().forEach(track => {
-            track.stop();
-            });
-        }
-
-    if(rtcPeerConnection){
-        rtcPeerConnection.ontrack=null;
-        rtcPeerConnection.onicecandidate=null;
-        rtcPeerConnection.close();
-        rtcPeerConnection=null;
-    }
-    
-}
-
-
-
-
-
-
 
 // Dots
 // ====
@@ -282,5 +281,3 @@ function animate(element, className) {
 // =========
 
 animate(dots, "dots--animate");
-
-
--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 // require('dotenv').config();
 
-
 const express = require("express");
 const socket = require("socket.io");
-const app=express();
-// var fs = require('fs');
-var http = require('http');
-// var https = require('https');
-// var credentials = {key: fs.readFileSync('key.pem'),
-// cert: fs.readFileSync('cert.pem')};
-
+const app = express();
+// var fs = require("fs");
+var http = require("http");
+// var https = require("https");
+// var credentials = {
+//   key: fs.readFileSync("key.pem"),
+//   cert: fs.readFileSync("cert.pem"),
+// };
 var httpServer = http.createServer(app);
 // var httpsServer = https.createServer(credentials, app);
 
+const server = httpServer.listen(process.env.PORT || 4000, (err) => {
+  if (err) console.log(err);
+  else console.log("Server Started at port 4000");
+});
 
-const server=httpServer.listen(process.env.PORT || 4000,(err)=>{
-    if(err)console.log(err);
-    else
-    console.log("Server Started at port 4000");
-})
+// app.use(express.static('Public'));
 
-app.use(express.static('Public'));
+// var io = socket(server, {
+//   cors: {
+//     origin: "http://127.0.0.1:5500/",
+//     credentials: true,
+//   },
+// });
+var io = socket(server);
 
-var io= socket(server);
+io.on("connection", (socket) => {
+  console.log("Connected: ", socket.id);
 
-io.on('connection',(socket)=>{
-    console.log('Connected: ',socket.id);
+  socket.on("join", (roomName) => {
+    var rooms = io.sockets.adapter.rooms;
+    var room = io.sockets.adapter.rooms.get(roomName);
 
-    socket.on('join',(roomName)=>{
-        var rooms=io.sockets.adapter.rooms;
-        var room=io.sockets.adapter.rooms.get(roomName);
+    if (room == undefined) {
+      socket.join(roomName);
+      socket.emit("created");
+    } else if (room.size == 1) {
+      socket.join(roomName);
+      socket.emit("joined");
+    } else {
+      socket.emit("full");
+    }
+    console.log("rooms-->", rooms);
+  });
 
-        if(room== undefined){
-            socket.join(roomName);
-            socket.emit('created');
-        }
-        else if(room.size==1){
-            socket.join(roomName);
-            socket.emit('joined');
-        }
-        else{
-            socket.emit('full');
-        }
-        console.log('rooms-->',rooms);
+  socket.on("ready", (roomName) => {
+    console.log("ready");
+    socket.broadcast.to(roomName).emit("ready");
+  });
 
-    })
+  socket.on("candidate", (candidate, roomName) => {
+    console.log("candidate");
+    socket.broadcast.to(roomName).emit("candidate", candidate);
+  });
 
-    socket.on('ready',(roomName)=>{
-        console.log('ready');
-        socket.broadcast.to(roomName).emit('ready');
-    });
+  socket.on("offer", (offer, roomName) => {
+    console.log("offer");
+    socket.broadcast.to(roomName).emit("offer", offer);
+  });
 
-    socket.on('candidate',(candidate,roomName)=>{
-        console.log('candidate');
-        socket.broadcast.to(roomName).emit('candidate',candidate);
-    });
+  socket.on("answer", (answer, roomName) => {
+    console.log("answer");
+    socket.broadcast.to(roomName).emit("answer", answer);
+  });
 
-    socket.on('offer',(offer,roomName)=>{
-        console.log('offer');
-        socket.broadcast.to(roomName).emit('offer',offer);
-    });
-
-    socket.on('answer',(answer,roomName)=>{
-        console.log('answer');
-        socket.broadcast.to(roomName).emit('answer',answer);
-    });
-
-    socket.on('endCall',(roomName)=>{
-        console.log('endCall');
-        socket.leave(roomName);
-        socket.broadcast.to(roomName).emit('endCall');
-    })
-
-})
+  socket.on("endCall", (roomName) => {
+    console.log("endCall");
+    socket.leave(roomName);
+    socket.broadcast.to(roomName).emit("endCall");
+  });
+});
